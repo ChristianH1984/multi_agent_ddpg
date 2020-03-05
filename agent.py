58054781d9a6e505d4e45df5ebca880c1f95a305
Christian <christian.hendricks@gmx.de>
2020-03-05 18:08:19 +0100

    WIP.

--- agent.py
+++ agent.py
@@ -168,3 +168,123 @@ class StochasticActorCriticAgent(BaseAgent):
 
 		self.soft_update(self.critic_local, self.critic_target)
 		self.soft_update(self.actor_local, self.actor_target)
+
+
+class MultiAgentDeterministicActorCriticAgent(BaseAgent):
+	"""Interacts with and learns from the environment."""
+
+	def __init__(self, gamma, tau, batch_size, update_every, actors, critics, memory, noise):
+		"""Initialize an Agent object.
+
+		Params
+		======
+			gamma (float): discount factor of future rewards
+			tau (float): soft update parameter
+			batch_size (int): number of samples for each mini batch
+			update_every (int): number of steps until weights are copied  from local to target Q-network
+			state_size (int): dimension of each state
+			action_size (int): dimension of each action
+			seed (int): random seed
+		"""
+		self.gamma = gamma
+		self.tau = tau
+		self.batch_size = batch_size
+		self.update_every = update_every
+		self.critics_local = critics
+		self.critics_target = [critic.clone() for critic in critics]
+
+		self.actors_local = actors
+		self.actors_target = [actor.clone() for actor in actors]
+
+		self.noise = noise
+
+		# Replay memory
+		self.memory = memory
+
+		# Initialize time step (for updating every UPDATE_EVERY steps)
+		self.t_step = 0
+
+	def act(self, state):
+		"""Returns actions for given state as per current policy.
+
+		Params
+		======
+			state (array_like): current state
+			eps (float): epsilon, for epsilon-greedy action selection
+		"""
+		state = torch.from_numpy(state).float().to(device)
+		for actor in self.actors_local:
+			actor.eval()
+		with torch.no_grad():
+			actions = self._compute_next_actions(self, self.actors_local, state)#self.actor_local(state)
+		for actor in self.actors_local:
+			actor.train()
+		actions = actions.cpu().data.numpy()
+		actions += self.noise.sample().reshape(-1, actions.shape[1])
+		return np.clip(actions, -1, 1)
+
+	def learn(self, experiences):
+		"""Update value parameters using given batch of experience tuples.
+
+		Params
+		======
+			experiences (Tuple[torch.Variable]): tuple of (s, a, r, s', done) tuples
+		"""
+		states, actions, rewards, next_states, dones = experiences
+
+		#next_actions = self.actor_target(next_states)
+		next_actions = self._compute_next_actions(self.actor_target, next_states)
+
+		next_states_actions = torch.cat((next_states.permute(1, 0, 2).reshape(self.batch_size, -1),
+										 next_actions.permute(1, 0, 2).reshape(self.batch_size, -1)), 1)
+		states_actions = torch.cat((states.float().permute(1, 0, 2).reshape(self.batch_size, -1),
+									actions.float().permute(1, 0, 2).reshape(self.batch_size, -1)), 1)
+
+		for i in range(len(self.critic_local)):
+			self._train_critic(self.critic_local[i], self.critic_target[i], states_actions, next_states_actions, rewards[i], dones[i])
+
+		for i in range(len(self.actor_local)):
+			self._train_actor(self.actor_local[i], self.actor_target[i], states[i])
+		#y_target = 100 * rewards + self.gamma * self.critic_target(next_states_actions)*(1 - dones)
+		#critic_error = 0.5*((y_target.detach() - self.critic_local(states_actions)) ** 2).mean()
+
+		#self.critic_local.optimizer.zero_grad()
+		#critic_error.backward()
+		#torch.nn.utils.clip_grad_norm(self.critic_local.parameters(), 1)
+		#self.critic_local.optimizer.step()
+
+		#actions = self.actor_local(states)
+		#states_actions = torch.cat((states.float(), actions.float()), 1)
+		#action_error = -self.critic_local(states_actions).mean()
+		#self.actor_local.optimizer.zero_grad()
+		#action_error.backward()
+		#self.actor_local.optimizer.step()
+
+		#self.soft_update(self.critic_local, self.critic_target)
+		#self.soft_update(self.actor_local, self.actor_target)
+
+	def _train_actor(self, actor_local, actor_target, states):
+		actions = actor_local(states)
+		states_actions = torch.cat((states.float(), actions.float()), 1)
+		action_error = -self.critic_local(states_actions).mean()
+		actor_local.optimizer.zero_grad()
+		action_error.backward()
+		actor_local.optimizer.step()
+
+		self.soft_update(actor_local, actor_target)
+
+	def _train_critic(self, critic_local, critic_target, states_actions, next_states_actions, rewards, dones):
+
+		y_target = 100 * rewards + self.gamma * critic_target(next_states_actions) * (1 - dones)
+		critic_error = 0.5 * ((y_target.detach() - critic_local(states_actions)) ** 2).mean()
+
+		critic_local.optimizer.zero_grad()
+		critic_error.backward()
+		torch.nn.utils.clip_grad_norm(critic_local.parameters(), 1)
+		critic_local.optimizer.step()
+
+		self.soft_update(critic_local, critic_target)
+
+
+	def _compute_next_actions(self, actors, next_states):
+		return np.stack([actors[i](next_states[i]) for i in range(len(actors))], axis=0)
